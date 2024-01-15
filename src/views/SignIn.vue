cc7ea6f127c53e650c2cbab12acb700c17365b23
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-01-15 13:59:01 -0500

    some stuff added to views

---
+++ src/views/SignIn.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        test2
+    </div>
+</template>
+  
+<script>
+
+</script>
+  
+<style>
+  
+
+</style>
+  
---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        test3
+    </div>
+</template>
+  
+<script>
+
+</script>
+  
+<style>
+  
+
+</style>
+  
