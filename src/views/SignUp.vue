0b06757145c145a7425770610a03dbab7672711c
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-01-16 13:14:31 -0500

    connected backend to frontend, allowing user login/registration

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,17 +1,44 @@
 <template>
-    <div>
-        Sign Up
-    </div>
+  <div>
+
+    <input v-model="username" placeholder="Username">
+    <br/>
+
+    <input v-model="email" placeholder="Email">
+    <br/>
+
+    <input type="password" v-model="password" placeholder="Password">
+    <br/>
+
+    <button @click="signup">Sign Up</button>
+
+  </div>
 </template>
-  
+
 <script>
+import axios from 'axios';
+
 export default {
-    
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    signup() {
+      axios.post('http://localhost:5052/api/auth/signup', {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      })
+      .then(response => {
+        localStorage.setItem('token', response.data.token);
+        this.$router.push('/'); 
+      })
+      .catch(error => console.error(error));
+    }
+  }
 }
 </script>
-  
-<style>
-  
-
-</style>
-  
