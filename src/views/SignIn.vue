f3824ed1c2aae7c7b1558045829761fb54352a91
Kmunoz2022 <kmunoz2022@gmail.com>
2024-04-25 00:08:46 -0400

    Sign in page and sign up

---
+++ src/views/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>SignIn</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>SignUp</div>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+
+
+  </style>
+  
