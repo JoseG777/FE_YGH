2950a65dfb22768345e0cc64d6d4b875f74c3bf5
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-24 23:16:54 -0400

    restarted

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,30 @@
-<template>
-
-  <div id="app">
-
-    <NavBar/>
-
-    <div class="page-content">
-
-      <router-view/>
-      
-    </div>
-
-  </div>
-
-</template>
-
-<script>
-import NavBar from './components/NavBar.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavBar
-  }
-}
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-html, body, #app {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  min-height: 100vh; /* Ensures the app fills at least the whole viewport */
-  width: 100%;
-}
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #ffffff;
-  background-color: #333333;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-#app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start; /* Changed to flex-start to start content after navbar */
-  text-align: center;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-.page-content {
-  width: 100%; /* Full width */
-  max-width: 960px; /* Maximum width, can adjust as needed */
-  margin-top: 60px; /* Push content down from the navbar */
-  flex: 1; /* Flex grow to take up remaining space */
-  display: flex;
-  align-items: center; /* Aligns vertically */
-  justify-content: center; /* Aligns horizontally */
-  padding: 20px; /* Padding inside the content area */
-  box-sizing: border-box;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
-
 </style>
