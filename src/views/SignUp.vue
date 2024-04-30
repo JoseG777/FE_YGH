30046af05da903cc27441fe6745a5d65f29244f7
Kmunoz2022 <kmunoz2022@gmail.com>
2024-04-30 00:12:20 -0400

    Signup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        <div>SignUp</div>
-    </div>
+  <div>
+    <h2>Sign Up</h2>
+    <form @submit.prevent="handleSignUp">
+      <input type="email" v-model="email" placeholder="Email">
+      <input type="password" v-model="password" placeholder="Password">
+      <button type="submit">Sign Up</button>
+    </form>
+  </div>
   </template>
   
   <script setup>
-  
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const email = ref('');
+const password = ref('');
+
+const handleSignUp = async () => {
+  const auth = getAuth();
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+    console.log("Sign up successful", userCredential.user);
+  } catch (error) {
+    console.error("Error signing up:", error.code, error.message);
+  }
+};
   </script>
   
   <style scoped>
