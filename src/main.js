2e4e5ed618342c407c88166fa8672d39fefb8747
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-30 00:23:10 -0400

    updated main.js to include firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import router from './router';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID,
+    measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
+};
+
+initializeApp(firebaseConfig);
+if (import.meta.env.VITE_APP_MEASUREMENT_ID) {
+    getAnalytics();
+    getStorage();
+}
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
