6c33e2861fd48a5e4abcf9a28f2180516469884a
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-05-07 00:55:58 -0400

    updated home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,17 @@
 <template>
-    <div>
-      <h1> Hello, Yu-Gi-Oh </h1>
-    </div>
+  <div>
+    <h1> Hello, Yu-Gi-Oh </h1>
+    <p> Welcome to our Yu-Gi-Oh card saver web app! </p>
+    <p> Currently this web app is a work in progress. We're planning to add the following:</p>
+    <ul>
+      <li> Better styling! We're focusing on functionality for now </li>
+      <li> Creating / uploading decks for everyone to see </li>
+      <li> Adding favorites </li>
+      <li> Deleting cards from saved </li>
+    </ul>
+  </div>
 </template>
-  
-<script setup>
 
-</script>
-  
-<style scoped>
+<script setup></script>
 
-</style>
-  
+<style scoped></style>
